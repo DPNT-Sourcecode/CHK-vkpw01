10623840fe86b04b05115cce1a0bfba7221982ea
sbx_user1051 <sbx_user1051@169.254.110.205>
2024-06-02 18:11:21 +0000

    Sun Jun 02 18:11:21 UTC 2024

---
+++ src/main/java/befaster/solutions/common/strategy/SpecialOfferPricingStrategy.java
@@ -0,0 +1,20 @@
+package befaster.solutions.common.strategy;
+
+import befaster.solutions.common.dto.Discount;
+
+public class DiscountedPricingStrategy implements PricingStrategy {
+    private final int unitPrice;
+    private final Discount discount;
+
+    public DiscountedPricingStrategy(int unitPrice, Discount discount) {
+        this.unitPrice = unitPrice;
+        this.discount = discount;
+    }
+
+    @Override
+    public int calculatePrice(int count) {
+        int roundsOfDiscount = count / discount.quantity();
+        int remainingItems = count % discount.quantity();
+        return roundsOfDiscount * discount.price() + remainingItems * unitPrice;
+    }
+}
